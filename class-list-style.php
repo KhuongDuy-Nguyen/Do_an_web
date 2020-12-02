7eeb3402264e04e7817b08596c8cc810712a496d
vincent2512 <74860718+vincent2512@users.noreply.github.com>
2020-12-02 20:39:01 +0700

    css

---
+++ class-list-style.php
@@ -0,0 +1,43 @@
+<style type="text/css">
+	.class-list-content {
+		background: #a8d4ff;
+	    height: 100vh;
+	    border: 1px solid;
+	    border-radius: 13px;
+	    padding-top: 15px;
+	    margin-top: 20px;
+	    overflow-y: auto;
+	}
+
+	.class-list-content .class-list-header {
+		background: #cdd826;
+	    margin-left: 25px;
+	    margin-right: 25px;
+	    font-weight: bold;
+	}
+
+	.class-list-content .class-list-header div{
+		border: 1px solid;
+	}
+
+	.class-list-content a.them {
+		margin-right: 25px;
+	    margin-bottom: 10px;
+	}
+
+	.class-list-content .class-list-body {
+		margin-left: 25px;
+	    margin-right: 25px;
+	}
+
+	.class-list-content .class-list-body input, .class-list-content .class-list-body a {
+		height: 30px;
+	    font-size: 12px;
+	    margin-top: 5px;
+	    margin-bottom: 5px;
+	}
+
+	.class-list-content .class-list-body div {
+		border: 1px solid;
+	}
+</style>
---
+++ class-registration-style.php
@@ -0,0 +1,24 @@
+<style type="text/css">
+	.class-registration-content {
+		background: #a8d4ff;
+	    height: 100vh;
+	    border: 1px solid;
+	    border-radius: 13px;
+	    padding-top: 15px;
+	    margin-top: 20px;
+	}
+
+	.class-registration-content .row {
+		margin-right: 0px;
+	    margin-left: 0px;
+	    padding: 10px;
+	}
+
+	.class-registration-content .row input {
+		width: 300px;
+	}
+
+	.class-registration-content .row input.btn {
+		width: 125px;
+	}
+</style>
